5d02b3988ac97ada80f85de9ebd0a47e5534860f
Bruno MacM3 <wbrunovieira77@gmail.com>
2025-12-29 16:16:01 -0300

    refactor: extract game mode UI components from human-body 3D environment

--- src/components/3d-environments/human-body/components/ui/index.ts
+++ src/components/3d-environments/human-body/components/ui/index.ts
@@ -2,3 +2,8 @@
 export { BodyPartButton } from './BodyPartButton';
 export { HotspotMenuItem } from './HotspotMenuItem';
 export { FullscreenButton } from './FullscreenButton';
+export { ModeToggle } from './ModeToggle';
+export { ChallengeModeUI } from './ChallengeModeUI';
+export { ConsultationModeUI } from './ConsultationModeUI';
+export { ScriviModeUI } from './ScriviModeUI';
+export { BodyPartsPanel } from './BodyPartsPanel';
